db584286534b1a06d1616b8e5f4497468af7ca1d
AngeRom <135559333+AngeRom@users.noreply.github.com>
2023-06-04 22:37:10 -0400

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    //alert("Bienvenidos");
+
+    /*console.log("Estoy utilizando la consola"); */
+});
+
+//let nombre = "Sebastián Parra";
+let numero1 = 100;
+let numero2 = 50;
+
+//console.log(numero1 + numero2);
+
+function sumarnumeros(num1, num2){
+    let resultado = num1 + num2;
+    console.log(resultado);
+}
+
+//sumarnumeros(numero1, numero2);
+sumarnumeros(4, "Seba");
